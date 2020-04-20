c7452973ca9a25a5af5639314598b15d6a80c3c1
Igor <garikus89@yandex.ru>
2020-04-21 00:02:09 +0300

    add mail send

--- script.js
+++ script.js
@@ -25,5 +25,20 @@ window.addEventListener('DOMContentLoaded', () => {
     //Mask
 
     $('input[name=number]').mask("+7 (999) 999-99-99");
+
+    $('form').submit(function(e){
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function(){
+            $(this).find("input").val("");
+            $('#consultation').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 });
 
